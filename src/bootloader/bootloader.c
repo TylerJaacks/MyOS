16cbff21cae9dbcf36c21c42746daad8c7930356
Tyler Jaacks <TylerJaacks@Gmail.com>
2022-09-19 22:15:42 -0500

    Added the ability to a load "kernel.elf".

--- src/bootloader/bootloader.c
+++ src/bootloader/bootloader.c
@@ -1,10 +1,32 @@
 #include <efi.h>
 #include <efilib.h>
  
-EFI_STATUS
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  EFI_FILE* LoadedFile;
+  EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+
+  SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**) &LoadedImage);
+  SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**) &FileSystem);
+
+  if (Directory == NULL) FileSystem->OpenVolume(FileSystem, &Directory);
+
+  EFI_STATUS status = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+
+  if (status != EFI_SUCCESS) return NULL;
+
+  return LoadedFile;
+}
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   InitializeLib(ImageHandle, SystemTable);
-  
-  Print(L"Hello, world!\n");
+
+  Print(L"Hello, world!\n\r");
+
+  if (LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable) == NULL)
+    Print(L"ERROR: Couldn't load kernel.elf\n\r");
+  else
+    Print(L"Kernel loaded successfully!\n\r");
+
+  return EFI_SUCCESS;
 }
